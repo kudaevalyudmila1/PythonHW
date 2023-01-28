879911af07db0e5b7358ec8ee08d0c6cd354a174
Lyudmila <kudaevalyudmila@gmail.com>
2023-01-29 00:16:31 +0300

    Task2

--- Seminar1/HomeTask2.py
+++ Seminar1/HomeTask2.py
@@ -1,9 +1,8 @@
 # Найдите сумму цифр трехзначного числа. 
 
 number = int(input('Введите число: '))
-print(number)
 if  99 < number < 1000:
     res = number // 100 + number // 10 % 10 + number % 10
 else:
     res = ('введено не трехзначное число')
-print(res)
+print('{} -> {}'.format(number, res))
