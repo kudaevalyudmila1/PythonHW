4c93580d797e27e6306e6703df662d09c601c457
Lyudmila <kudaevalyudmila@gmail.com>
2023-02-02 15:43:10 +0300

    Task9

--- Seminar1/HomeTask8.py
+++ Seminar1/HomeTask8.py
@@ -12,3 +12,36 @@ if k < n and k < m or (k % m != 0 and k % n != 0) :
 else:
     res = ('YES')
 print('{}, {}, {},-> {}'.format(m, n, k, res))
+
+# По данному целому неотрицательному n вычислите
+# значение n!. N! = 1 * 2 * 3 * … * N (произведение всех
+# чисел от 1 до N) 0! = 1 Решить задачу используя цикл
+# while
+number = int(input('Введите натуральное число: '))
+
+if number >= 0:
+    result = 1
+    n = 1
+    while n <= number:
+        result = result * n
+        n = n + 1
+else:
+    print('Введено ненатуральное число')
+print(f'Факториал числа {number} = {result}')
+
+x = int(input('Введите число: '))
+
+
+# def factorial(n):
+# res = 1
+# i = 1
+# while i <= n:
+# if n > 0:
+# res *= i
+# i += 1
+# if n == 0:
+# print(res)
+# print(res)
+
+
+# factorial(x)
