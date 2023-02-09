5ac47748b1dbd80374fd66292049dc3579364c3d
Lyudmila <kudaevalyudmila@gmail.com>
2023-02-09 13:12:55 +0300

    Task27

---
+++ Seminar4/Task27.py
@@ -0,0 +1,17 @@
+# Пользователь вводит текст(строка). Словом считается
+# последовательность непробельных символов идущих
+# подряд, слова разделены одним или большим числом
+# пробелов. Определите, сколько различных слов
+# содержится в этом тексте.
+# Input: She sells sea shells on the sea shore The shells
+# that she sells are sea shells I'm sure.So if she sells sea
+# shells on the sea shore I'm sure that the shells are sea
+# shore shells
+# Output: 13
+
+input_str = "She sells sea shells on the sea shore The shells that she sells are sea shells I'm sure So if she sells sea shells on the sea shore I'm sure that the shells are sea shore shells"
+
+str_list = input_str.lower().split()
+res_str = set(str_list)
+print(res_str)
+print(len(res_str))
