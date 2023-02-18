5cdb9fbe78cade5bc1b4a596d5ad1c305287fdec
Lyudmila <kudaevalyudmila@gmail.com>
2023-02-18 19:20:17 +0300

    Task30

--- Seminar6/HomeTask30.py
+++ Seminar6/HomeTask30.py
@@ -6,3 +6,17 @@
 #  = a1
 #  + (n-1) * d.
 # Каждое число вводится с новой строки.
+
+
+def arithmetic_progression(n, a1, d):
+    list_1 = []
+    list_1.append(a1)
+    for i in range(1, n):
+        list_1.append(a1 + i * d)
+    return list_1
+
+
+num = int(input('Введите количество элементов  прогрессии:'))
+a_1 = int(input('Введите первый  элемент  прогрессии:'))
+step = int(input('Введите разность между соседними элементами  прогрессии:'))
+print(arithmetic_progression(num, a_1, step))
