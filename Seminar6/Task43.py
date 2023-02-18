eaf3ba327c566efe37df9135ae5951c61813140c
Lyudmila <kudaevalyudmila@gmail.com>
2023-02-18 17:01:17 +0300

    Task43

--- Seminar6/Task43.py
+++ Seminar6/Task43.py
@@ -4,3 +4,22 @@
 # которую необходимо посчитать. Вводится список
 # чисел. Все числа списка находятся на разных
 # строках.
+
+def number_list(x):
+    list_1 = []
+    for _ in range(x):
+        list_1.append(int(input('Введите элемент массива: ')))
+    return list_1
+
+
+def count_pair(list_1: list):
+    sum_count = 0
+    for i in set(list_1):
+        sum_count += list_1.count(i) // 2
+    return sum_count
+
+
+n = int(input('Введите количество элементов  списка:'))
+list_1 = number_list(n)
+print(list_1)
+print(count_pair(list_1))
