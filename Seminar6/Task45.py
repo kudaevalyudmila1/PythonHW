d71f233be55d6489f70d4469a7d99e8c7cd577e1
Lyudmila <kudaevalyudmila@gmail.com>
2023-02-18 18:44:28 +0300

    Task45

--- Seminar6/Task45.py
+++ Seminar6/Task45.py
@@ -12,3 +12,21 @@
 # строке, разделяя пробелами. Каждая пара должна быть
 # выведена только один раз (перестановка чисел новую
 # пару не дает).
+
+def sum_divisors(num):
+    summ = 0
+    for i in range(1, num // 2 + 1):
+        if num % i == 0:
+            # print(f'{i = }')
+            summ += i
+    return summ
+
+
+def friends_num(number):
+    for i in range(1, number):
+        j = sum_divisors(i)
+        if i < j <= number and i == sum_divisors(j):
+            print(i, j)
+
+
+friends_num(int(input('Введите число К: ')))
